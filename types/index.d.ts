7a4cb350d887ba6e1f59f161fb6acfcb4e252882
cezzzanne <pablo.arellanos97@gmail.com>
2023-10-16 19:53:42 -0400

    add types

---
+++ types/index.d.ts
@@ -0,0 +1,12 @@
+declare module 'rn-ios-push-provisioning' {
+    import { Component } from 'react';
+    import { ViewStyle } from 'react-native';
+  
+    interface RNRedButtonProps {
+      style?: ViewStyle;
+      // Add any other props here
+    }
+  
+    export default class RNRedButton extends Component<RNRedButtonProps> {}
+  }
+  
