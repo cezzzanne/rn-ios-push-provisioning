ccdc5d71de914f798301f2ff4697078840a26a32
cezzzanne <pablo.arellanos97@gmail.com>
2023-10-17 17:34:49 -0400

    Check if we can add payment pass

--- ios/RNPushProvisioning/RNPushProvisioning/RNPushProvisioning.m
+++ ios/RNPushProvisioning/RNPushProvisioning/RNPushProvisioning.m
@@ -7,6 +7,7 @@
 
 #import "RNPushProvisioning.h"
 #import <React/RCTLog.h>
+#import <PassKit/PassKit.h>
 
 @implementation RNPushProvisioning
 
@@ -14,6 +15,16 @@ RCT_EXPORT_MODULE();
 
 RCT_EXPORT_METHOD(createCalendarEvent:(NSString *)name location:(NSString *)location) {
      RCTLogInfo(@"Pretending to create an event %@ at %@", name, location);
+}
+
+RCT_EXPORT_METHOD(checkAvailability:(NSString *)name location:(NSString *)location) {
+    if (![PKAddPaymentPassViewController canAddPaymentPass]) {
+        // Your device cannot add payment passes
+        RCTLogInfo("Cannot add payment pass");
+    } else {
+        // Your device can add payment passes
+        RCTLogInfo("Can add payment pass");
+    }
 }
 
 @end
