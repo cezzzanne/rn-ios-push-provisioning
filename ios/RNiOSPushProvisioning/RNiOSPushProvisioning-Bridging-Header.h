d043bfa74457e5cb7606aef2814bd5215e733b0c
cezzzanne <pablo.arellanos97@gmail.com>
2023-10-17 14:35:24 -0400

    Add bridging header

---
+++ ios/RNiOSPushProvisioning/RNiOSPushProvisioning-Bridging-Header.h
@@ -0,0 +1,14 @@
+//
+//  RNiOSPushProvisioning-Bridging-Header.h
+//  RNiOSPushProvisioning
+//
+//  Created by Pablo Arellano on 10/17/23.
+//
+// CalendarManager-Bridging-Header.h
+#import <React/RCTBridgeModule.h>
+
+#ifndef RNiOSPushProvisioning_Bridging_Header_h
+#define RNiOSPushProvisioning_Bridging_Header_h
+
+
+#endif /* RNiOSPushProvisioning_Bridging_Header_h */
