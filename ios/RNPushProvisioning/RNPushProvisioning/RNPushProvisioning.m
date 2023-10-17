bcc7e02898ccb4b37605e9313142da9a5dd5a39c
cezzzanne <pablo.arellanos97@gmail.com>
2023-10-17 18:14:25 -0400

    present view controller for add payment pass

--- ios/RNPushProvisioning/RNPushProvisioning/RNPushProvisioning.m
+++ ios/RNPushProvisioning/RNPushProvisioning/RNPushProvisioning.m
@@ -14,12 +14,21 @@
 RCT_EXPORT_MODULE();
 
 RCT_EXPORT_METHOD(checkAvailability:(NSString *)name location:(NSString *)location) {
-    if (![PKAddPaymentPassViewController canAddPaymentPass]) {
+    if ([PKAddPaymentPassViewController canAddPaymentPass]) {
         // Your device cannot add payment passes
-        RCTLogInfo("Cannot add payment pass");
+        PKAddPaymentPassRequestConfiguration *configuration = [[PKAddPaymentPassRequestConfiguration alloc] initWithStyle:PKAddPaymentPassStylePayment];
+        configuration.localizedDescription = @"Tarjeta Nelo Test";
+        configuration.primaryAccountSuffix = @"1234";
+        configuration.cardholderName = @"Johnny Thunders";
+        configuration.paymentNetwork = @"Visa";
+        configuration.encryptionScheme = PKEncryptionSchemeECC_V2;
+        PKAddPaymentPassViewController *addPaymentVC = [[PKAddPaymentPassViewController alloc] initWithRequestConfiguration:configuration delegate:self];
+        [self presentViewController:addPaymentVC animated:YES completion:nil];
+
+        RCTLogInfo("Can add payment pass");
     } else {
         // Your device can add payment passes
-        RCTLogInfo("Can add payment pass");
+        RCTLogInfo("Cannot add payment pass");
     }
 }
 
